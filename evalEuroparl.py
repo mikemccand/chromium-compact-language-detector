2ce4341a00a04194bc60bb5a8b22082a744d89f7
Michael McCandless <lucene@mikemccandless.com>
2011-10-23 15:39:55 -0400

    print perf results

--- evalEuroparl.py
+++ evalEuroparl.py
@@ -4,6 +4,7 @@
 
 import cld
 import sys
+import time
 
 # Download corpus at http://code.google.com/p/language-detection/downloads/detail?name=europarl-test.zip
 
@@ -16,17 +17,20 @@ f = open(corpusFile)
 count = 0
 fail = 0
 byLang = {}
+bytes = 0
+startTime = time.time()
 for line in f.readlines():
   count += 1
   answer, text = line.split('\t')
   if answer not in byLang:
     byLang[answer] = {}
+  bytes += len(text)
   name, code, reliable, numBytes, details = cld.detect(text, isPlainText=True, removeWeakLanguages=False, pickSummaryLanguage=False)
   byLang[answer][code] = 1 + byLang[answer].get(code, 0)
   if code != answer:
     #print 'WRONG: %d: %s (%s) vs %s [reliable=%s numBytes=%s details=%s]' % (count, code, name, answer, reliable, numBytes, details)
     fail += 1
-
+elapsedSec = time.time() - startTime
 # print 'FAIL %d count %d' % (fail, count)
 l = byLang.items()
 l.sort()
@@ -45,6 +49,6 @@ for lang, hits in l:
   print '%s %5.1f%%: %s %s' % (lang, 100.0*correctCT/sum, s0, ' '.join('%s=%d' % (lang2, ct) for ct, lang2 in l2[1:]))
 
 print
-print 'Total %.2f%% (= %d/%d)' % (100.0*(count-fail)/count, count-fail, count)
+print 'Total %.2f%% (= %d/%d); %d bytes in %.1f msec (%.1f MB/sec)' % (100.0*(count-fail)/count, count-fail, count, bytes, 1000.0*elapsedSec, bytes/1024./1024./elapsedSec)
 
 assert totFail == fail
