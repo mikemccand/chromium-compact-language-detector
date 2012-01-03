96730b715a172b8fc7b7d5b47fcd390c2fcb04a8
Michael McCandless <lucene@mikemccandless.com>
2012-01-03 09:38:46 -0500

    issue 6: let setup.py compile all CLD sources (don't separately pre-compile libcld.a)

--- setup.py
+++ setup.py
@@ -2,6 +2,26 @@ from distutils.core import setup, Extension
 
 import platform
 
+CLD_SOURCES = ["encodings/compact_lang_det/cldutil.cc",
+               "encodings/compact_lang_det/cldutil_dbg_empty.cc",
+               "encodings/compact_lang_det/compact_lang_det.cc",
+               "encodings/compact_lang_det/compact_lang_det_impl.cc",
+               "encodings/compact_lang_det/ext_lang_enc.cc",
+               "encodings/compact_lang_det/getonescriptspan.cc",
+               "encodings/compact_lang_det/letterscript_enum.cc",
+               "encodings/compact_lang_det/tote.cc",
+               "encodings/compact_lang_det/generated/cld_generated_score_quadchrome_0406.cc",
+               "encodings/compact_lang_det/generated/compact_lang_det_generated_cjkbis_0.cc",
+               "encodings/compact_lang_det/generated/compact_lang_det_generated_ctjkvz.cc",
+               "encodings/compact_lang_det/generated/compact_lang_det_generated_deltaoctachrome.cc",
+               "encodings/compact_lang_det/generated/compact_lang_det_generated_quadschrome.cc",
+               "encodings/compact_lang_det/win/cld_htmlutils_windows.cc",
+               "encodings/compact_lang_det/win/cld_unilib_windows.cc",
+               "encodings/compact_lang_det/win/cld_utf8statetable.cc",
+               "encodings/compact_lang_det/win/cld_utf8utils_windows.cc",
+               "encodings/internal/encodings.cc",
+               "languages/internal/languages.cc"]
+
 defines = [('CLD_WINDOWS', None)]
 
 if platform.system() == 'Windows':
@@ -9,11 +29,10 @@ if platform.system() == 'Windows':
 
 module = Extension('cld',
                    language='c++',
-                   libraries=['cld'],
                    include_dirs=['.'],
                    library_dirs=['.'],
                    define_macros = defines,
-                   sources=['pycldmodule.cc'])
+                   sources=['pycldmodule.cc'] + CLD_SOURCES)
 
 setup(name='cld',
       version='0.031415',
