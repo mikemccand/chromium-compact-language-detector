8ced8b496a43fc2f6aef1bbcc5689e1631f12a92
Michael McCandless <lucene@mikemccandless.com>
2012-05-17 07:08:03 -0400

    issue 9: name files so auto* tools are happier

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # Copyright (c) 2009 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+# found in the COPYING file.
 
 import cld
 import unittest
